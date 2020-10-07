4405b70ccf171cb04e0cd4728c782be20b25e552
r2d2-lex <r2d2-lex@yandex.ru>
2020-10-07 19:14:03 +0000

    добавил тест для update_currency_rate+моки

--- gqlshop/catalog/tasks.py
+++ gqlshop/catalog/tasks.py
@@ -1,18 +1,21 @@
 from django_rq import job
-from catalog.models import CurrencyRate
+from .models import CurrencyRate
 import requests
+
+
+def get_currency_rate(currency):
+    pair = '{}RUB'.format(currency.upper())
+    response = requests.get(
+        'https://www.freeforexapi.com/api/live?pairs={}'.format(pair)
+    )
+    data = response.json()
+    return data['rates'][pair]['rate']
 
 
 @job('default')
 def update_currency_rate():
     queryset = CurrencyRate.objects.all()
     for rate in queryset:
-        pair = '{}RUB'.format(rate.currency.upper())
-        response = requests.get(
-            'https://www.freeforexapi.com/api/live?pairs={}'.format(pair)
-        )
-    data = response.json()
-
-    rate.rate = data['rates'][pair]['rate']
-    rate.save(update_fields=['rate'])
+        rate.rate = get_currency_rate(rate.currency)
+        rate.save(update_fields=['rate'])
     return True
--- gqlshop/catalog/tests.py
+++ gqlshop/catalog/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import CurrencyRate
+from .tasks import get_currency_rate, update_currency_rate
+import mock
 
-# Create your tests here.
+
+class FakeResponse:
+    def json(self):
+        return {
+            "rates":
+                {
+                    "USDRUB":
+                        {
+                            "rate": 60.5,
+                            "timestamp": 1602076146
+                        }
+                },
+            "code": 200
+        }
+
+
+class CurrencyRateTestCase(TestCase):
+    @mock.patch('requests.get', return_value=FakeResponse())
+    def test_get_currency_rate(self, mocked_get):
+        rate = get_currency_rate('usd')
+        self.assertEqual(rate, 60.5, 'Rate value not equal to 60.5')
+        self.assertTrue(mocked_get.called)
+
+    @mock.patch('requests.get', return_value=FakeResponse())
+    def test_update_currency_rate(self, mocked_get):
+        rate = CurrencyRate.objects.create(currency='usd')
+        self.assertEqual(rate.rate, 1)
+
+        self.assertTrue(update_currency_rate())
+        rate = CurrencyRate.objects.get(currency='usd')
+        self.assertEqual(rate.rate, 60.5, 'Rate value not equal to 60.5')
+
+# Example Mock 1
+# class CurrencyRateTestCase(TestCase):
+#
+#     def test_get_currency_rate(self):
+#         with mock.patch('requests.get', return_value=FakeResponse()):
+#             rate = get_currency_rate('usd')
+#             self.assertEqual(rate, 60.5, 'Rate value not equal to 60.5')
